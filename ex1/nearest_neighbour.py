1c16cfa2f313b43f2f7ce365b3822131e490cac7
OmriStudies <baroz.omri@gmail.com>
2022-11-22 14:27:01 +0200

    finished everything

--- ex1/nearest_neighbour.py
+++ ex1/nearest_neighbour.py
@@ -4,6 +4,7 @@ from scipy.spatial import distance
 from matplotlib import pyplot as plt
 import tqdm
 from typing import List, TypeVar
+import random
 
 ExampleType = TypeVar("ExampleType")
 LabelType = TypeVar("LabelType")
@@ -36,6 +37,45 @@ class KNearestNeighborClassifier:
         return np.bincount(k_neighbors_labels).argmax()
 
 
+def choose_other(curr, l: list):
+    f"""
+    chooses element from list {l} that is different than {curr}
+    :param curr: 
+    :param l: 
+    :return: 
+    """
+    if curr in l:
+        l = [elem for elem in l if elem != curr] # new list without the current element
+    return random.choice(l)
+
+def gensmallm_corrupted(x_list: List[ExampleType], y_list: List[LabelType], m: int, rate: float = 0.15):
+    f"""
+    generates a random corrupted with rate of {rate}
+    :param x_list:
+    :param y_list:
+    :param m:
+    :return:
+    """
+    assert len(x_list) == len(y_list), 'The length of x_list and y_list should be equal'
+
+    x = np.vstack(x_list)
+    y = np.concatenate([y_list[j] * np.ones(x_list[j].shape[0]) for j in range(len(y_list))])
+
+    indices = np.arange(x.shape[0])
+    np.random.shuffle(indices)
+
+    rearranged_x = x[indices]
+    rearranged_y = y[indices]
+    rearranged_x = rearranged_x[:m]
+    rearranged_y = rearranged_y[:m]
+    for i in range(len(rearranged_y)):
+        if random.uniform(1,100) > rate*100:
+            continue
+        label = rearranged_y[i]
+        new_label = choose_other(label, y_list)
+        rearranged_y[i] = new_label
+    return rearranged_x, rearranged_y
+
 def gensmallm(x_list: List[ExampleType], y_list: List[LabelType], m: int):
     """
     gensmallm generates a random sample of size m along side its labels.
@@ -59,7 +99,6 @@ def gensmallm(x_list: List[ExampleType], y_list: List[LabelType], m: int):
     return rearranged_x[:m], rearranged_y[:m]
 
 
-# todo: complete the following functions, you may add auxiliary functions or define class to help you
 
 
 def learnknn(k: int, x_train: np.array, y_train: np.array):
@@ -132,6 +171,16 @@ def run_knn_mnist_sample_size(k: int, training_sample_size: int, x_test, y_test,
 
     return error
 
+def run_knn_mnist_sample_size_corrupted(k: int, training_sample_size: int, x_test, y_test, trains, labels) -> ndarray:
+    x_train, y_train = gensmallm_corrupted(trains, labels, training_sample_size)
+
+    classifer = learnknn(k, x_train, y_train)
+
+    preds = predictknn(classifer, x_test)
+    y_test = y_test.astype(int).reshape(preds.shape)
+    error = np.mean(y_test != preds)
+
+    return error
 
 def run_knn_mnist_k1():
     """
@@ -230,6 +279,51 @@ def run_knn_mnist(training_sample_size, k_min, k_max):
     plt.show()
 
 
+def run_knn_mnist_corrupted(training_sample_size, k_min, k_max):
+    data = np.load("mnist_all.npz")
+
+    test2 = data['test2']
+    test3 = data['test3']
+    test5 = data['test5']
+    test6 = data['test6']
+
+    train2 = data['train2']
+    train3 = data['train3']
+    train5 = data['train5']
+    train6 = data['train6']
+
+    to_train = [train2, train3, train5, train6]
+    labels = [2, 3, 5, 6]
+
+    test_length = test2.shape[0] + test3.shape[0] + test5.shape[0] + test6.shape[0]
+
+    x_test, y_test = gensmallm_corrupted([test2, test3, test5, test6], [2, 3, 5, 6], test_length)
+    each_sample_repetition = 10
+    avg_errors = []
+    errors_per_run = {}
+    min_errors = []
+    max_errors = []
+
+    for k in tqdm.tqdm(range(k_min, k_max+1)):
+        errors_per_run[k] = []
+        for _ in tqdm.tqdm(range(each_sample_repetition)):
+            error = run_knn_mnist_sample_size(k, training_sample_size, x_test, y_test, to_train, labels)
+            errors_per_run[k].append(error)
+        avg_errors.append(sum(errors_per_run[k]) / each_sample_repetition)
+        min_errors.append(min(errors_per_run[k]))
+        max_errors.append(max(errors_per_run[k]))
+
+    min_distance = [a - b for a, b in zip(avg_errors, min_errors)]
+    max_distance = [b - a for a, b in zip(avg_errors, max_errors)]
+    error_bar = np.array([min_distance, max_distance])
+    ks = list(range(k_min, k_max+1))
+    plt.plot(ks, avg_errors, color='blue', marker="o")
+    plt.errorbar(ks, avg_errors, yerr=error_bar, fmt="none", ecolor='red')
+    plt.xlabel("k")
+    plt.ylabel("Average Error")
+    plt.title(f"Average Error of KNearestNeighbors with Sample Size = {training_sample_size}")
+    plt.show()
+
 if __name__ == '__main__':
     # before submitting, make sure that the function simple_test runs without errors
     # simple_test()
@@ -238,7 +332,13 @@ if __name__ == '__main__':
     # run_knn_mnist_k1()
 
     # 2.e
+    # sample_size = 200
+    # k_min = 1
+    # k_max = 11
+    # run_knn_mnist(sample_size, k_min, k_max)
+
+    # 2.f
     sample_size = 200
     k_min = 1
     k_max = 11
-    run_knn_mnist(sample_size, k_min, k_max)
+    run_knn_mnist_corrupted(sample_size, k_min, k_max)
