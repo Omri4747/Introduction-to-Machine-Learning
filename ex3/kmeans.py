6071b97ebe0608a07a747948be5aa6af8c6519c1
OmriStudies <baroz.omri@gmail.com>
2023-01-13 16:18:58 +0200

    finished 1.a 1.b

--- ex3/kmeans.py
+++ ex3/kmeans.py
@@ -1,6 +1,29 @@
 import numpy as np
 
 
+def random_centrodis(X, k):
+    indexes = np.random.choice(range(X.shape[0]), size=k)
+    # centroids = [X[i] for i in indexes]
+    return X[indexes]
+
+
+def update_clusters(X, k, centroids):
+    distances = np.zeros((X.shape[0], k))
+    for i in range(k):
+        distances[:, i] = np.linalg.norm(X - centroids[i], axis=1)
+    clusters = np.argmin(distances, axis=1)
+    return clusters
+
+
+def update_centroids(X, k, clusters):
+    new_centroids = np.zeros((k, X.shape[1]))
+    for i in range(k):
+        cluster_i = X[clusters == i]
+        new_centroids[i] = np.mean(cluster_i, axis=0)
+
+    return new_centroids
+
+
 def kmeans(X, k, t):
     """
     :param X: numpy array of size (m, d) containing the test samples
@@ -8,7 +31,19 @@ def kmeans(X, k, t):
     :param t: the number of iterations to run
     :return: a column vector of length m, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned.
     """
-    raise NotImplementedError()
+    centroids: np.ndarray = random_centrodis(X, k)
+    clusters = None
+    for _ in range(t):
+        prev = clusters
+        clusters = update_clusters(X, k, centroids)
+        centroids = update_centroids(X, k, clusters)
+        if prev is None:
+            continue
+        is_same = prev == clusters
+        if is_same.all():
+            break
+
+    return clusters.reshape((clusters.shape[0], 1))
 
 
 def simple_test():
@@ -22,6 +57,7 @@ def simple_test():
 
     assert isinstance(c, np.ndarray), "The output of the function softsvm should be a numpy array"
     assert c.shape[0] == m and c.shape[1] == 1, f"The shape of the output should be ({m}, 1)"
+
 
 if __name__ == '__main__':
     # before submitting, make sure that the function simple_test runs without errors
--- ex3/singlelinkage.py
+++ ex3/singlelinkage.py
@@ -2,19 +2,55 @@ import numpy as np
 import scipy.io as sio
 
 
+def update_clusters(X, clusters, dist_matrix, curr_loop):
+    m = X.shape[0]
+    min_dist_idx = np.unravel_index(np.argmin(dist_matrix), (m, m))
+    a = min_dist_idx[0]
+    b = min_dist_idx[1]
+    for i in range(m):
+        if i != a and i != b:
+            temp = min(dist_matrix[a][i], dist_matrix[b][i])
+            dist_matrix[a][i] = temp
+            dist_matrix[i][a] = temp
+
+    # 'b' cluster merged into 'a'. Set dist from 'b' cluster to all other clusters to be infinity
+    dist_matrix[b, :] = np.inf
+    dist_matrix[:, b] = np.inf
+
+    clusters[clusters == b] = a
+
+    return clusters
+
+
 def singlelinkage(X, k):
     """
     :param X: numpy array of size (m, d) containing the test samples
     :param k: the number of clusters
     :return: a column vector of length m, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned.
     """
-    raise NotImplementedError()
+    m = X.shape[0]
+    dist_matrix = np.zeros((m, m))
+    for i in range(m):
+        for j in range(i):
+            dist = np.linalg.norm(X[i] - X[j])
+            dist_matrix[i, j] = dist
+            dist_matrix[j, i] = dist
+        # fill diagonal with infinity values, that way clusters would not choose itself for combining
+        dist_matrix[i, i] = np.inf
+
+    clusters = np.array(range(m))
+    for curr_loop in range(X.shape[0] - k):
+        clusters = update_clusters(X, clusters, dist_matrix, curr_loop)
+
+    return clusters.reshape((clusters.shape[0], 1))
 
 
 def simple_test():
     # load sample data (this is just an example code, don't forget the other part)
     data = np.load('mnist_all.npz')
-    X = np.concatenate((data['train0'], data['train1']))
+    data0 = data['train0']
+    data1 = data['train1']
+    X = np.concatenate((data0[np.random.choice(data0.shape[0], 30)], data1[np.random.choice(data1.shape[0], 30)]))
     m, d = X.shape
 
     # run single-linkage
